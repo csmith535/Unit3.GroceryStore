bddeb6c5528cbcbcfccaf3c6687de0fd095e1dd0
Cameron Smith <cameron.smith3030@gmail.com>
2025-08-06 22:43:30 -0400

    completed JSDoc instructions, used forEach/map/find/loop/filter/reduce(2)

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const inventory = [
  */
 function logNames(items) {
   // TODO: use `forEach`
+  items.forEach((item) => console.log(item.name));
 }
 
 /**
@@ -35,6 +36,7 @@ function logNames(items) {
  */
 function getUppercaseNames(items) {
   // TODO: use `map`
+  return items.map((item) => item.name.toUpperCase());
 }
 
 /**
@@ -44,6 +46,7 @@ function getUppercaseNames(items) {
  */
 function getItemById(items, id) {
   // TODO: use `find`
+  return items.find((item) => item.id === id);
 }
 
 /**
@@ -53,6 +56,12 @@ function getItemById(items, id) {
  */
 function getItemPriceByName(items, name) {
   // TODO: use a loop!
+  let promptItem = items.find((item) => item.name === name);
+
+  if (promptItem) {
+    return promptItem.price;
+  }
+  return null;
 }
 
 /**
@@ -62,6 +71,7 @@ function getItemPriceByName(items, name) {
  */
 function getItemsByCategory(items, category) {
   // TODO: use `filter`
+  return items.filter((item) => item.category === category);
 }
 
 /**
@@ -70,6 +80,7 @@ function getItemsByCategory(items, category) {
  */
 function countItems(items) {
   // TODO: use `reduce`
+  return items.reduce((quantity, item) => (quantity += item.quantity), 0);
 }
 
 /**
@@ -78,6 +89,13 @@ function countItems(items) {
  */
 function getTotalPrice(items) {
   // TODO: use `reduce`
+  return items.reduce(
+    (totalPrice, item) => (totalPrice += item.price * item.quantity),
+    0
+  );
+  //{
+  //   return totalPrice += item.price * item.quantity;
+  // }, 0);
 }
 
 // === READ BUT DO NOT CHANGE THE CODE BELOW ===
